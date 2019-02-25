ebd28b152ccfa9bea5f725a05ad330b1e237f355
Chris Cowell <chris@trufflesuite.com>
2019-02-25 11:20:43 -0500

    Check block gas limit when not using eth_call

--- test/gas/blockGasLimit.js
+++ test/gas/blockGasLimit.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const bootstrap = require("../helpers/contract/bootstrap");
 const randomInteger = require("../helpers/utils/generateRandomInteger");
 
-describe("Specifying a sender gas limit greater than block gas limitations", function() {
+describe.only("Specifying a sender gas limit greater than block gas limitations", function() {
   const SEED_RANGE = 1000000;
   let context;
 
@@ -26,7 +26,7 @@ describe("Specifying a sender gas limit greater than block gas limitations", fun
   const iterations = 10 ** 6;
   const clientGasLimit = 10 ** 8;
 
-  it("should generate a block gas limit error when calling a 'view' function", async function() {
+  it.only("should generate a block gas limit error when calling a 'view' function", async function() {
     const { instance, web3 } = context;
 
     const block = await web3.eth.getBlock("latest");
@@ -34,13 +34,8 @@ describe("Specifying a sender gas limit greater than block gas limitations", fun
     assert(clientGasLimit > block.gasLimit);
     assert(clientGasLimit < Number.MAX_SAFE_INTEGER);
 
-    try {
-      // Attempt to run an expensive view function
-      await instance.methods.expensiveOperation(iterations).call({ gas: clientGasLimit });
-      assert.fail("Should not be able to run an expensive 'view' function");
-    } catch (error) {
-      assert.strictEqual(error.message, "Exceeds block gas limit");
-    }
+    // Attempt to run an expensive view function
+    await instance.methods.expensiveOperation(iterations).call({ gas: clientGasLimit });
   });
 
   it("should generate a block gas limit error when calling a 'pure' function", async function() {
@@ -51,11 +46,7 @@ describe("Specifying a sender gas limit greater than block gas limitations", fun
     assert(clientGasLimit > block.gasLimit);
     assert(clientGasLimit < Number.MAX_SAFE_INTEGER);
 
-    try {
-      await instance.methods.pureExpensiveOperation(iterations).call({ gas: clientGasLimit });
-      assert.fail("Should not be able to run an expensive 'pure' function");
-    } catch (error) {
-      assert.strictEqual(error.message, "Exceeds block gas limit");
-    }
+    // Attempt to run an expensive pure function
+    await instance.methods.pureExpensiveOperation(iterations).call({ gas: clientGasLimit });
   });
 });
